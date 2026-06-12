d2014d12707df05dad7464371686d9230f3775be
ayushi <ayushinagar069@gmail.com>
2026-06-12 20:41:33 +0530

    first commit

---
+++ server/lib/utils.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+import jwt from "jsonwebtoken";
+
+// function to generate token for user
+
+export const generateToken = (userId) => {
+  const token = jwt.sign(
+    { userId },
+    process.env.JWT_SECRET,
+    { expiresIn: "7d" }
+  );
+
+  return token;
+};
